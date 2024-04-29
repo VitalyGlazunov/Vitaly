839e26a20606fcadb65e868c720f5fd8cca0dfa4
VitalyGlazunov <vitalyglazunoff@yandex.ru>
2024-04-29 13:58:06 +0300

    v5

--- quality_control/admin.py
+++ quality_control/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'project', 'priority')
+    search_fields = ('title', 'description')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'task')
+        }),
+        ('Availability', {
+            'fields': ('status', 'priority')
+        }),
+    )
+    list_editable = ['status']
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'project', 'priority')
+    search_fields = ('title', 'description')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'task')
+        }),
+        ('Availability', {
+            'fields': ('status', 'priority')
+        }),
+    )
