e98fa4bdddaacb7fce990aad09508f6f85fd7a14
VitalyGlazunov <vitalyglazunoff@yandex.ru>
2024-04-30 15:53:36 +0300

    v12

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.urls import reverse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import ProjectForm, TaskForm
 
 
 def index(request):
@@ -27,6 +28,31 @@ def project_detail(request, project_id):
 def task_detail(request, project_id, task_id):
     task = get_object_or_404(Task, id=task_id, project_id=project_id)
     return render(request, 'tasks/task_detail.html', {'task': task})
+
+
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:projects_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'tasks/project_create.html', {'form': form})
+
+
+def add_task_to_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = project
+            task.save()
+            return redirect('tasks:project_detail', project_id=project.id)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form, 'project': project})
 
 
 class IndexView(View):
