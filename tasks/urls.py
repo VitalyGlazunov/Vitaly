91c5b9e5e2ca9ed1131da877bf9e4e9afd73bde2
VitalyGlazunov <vitalyglazunoff@yandex.ru>
2024-04-30 11:54:23 +0300

    v10

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,10 +5,10 @@ app_name = 'tasks'
 
 urlpatterns = [
 
-    # path('', views.index),
-    # path('projects/', views.projects_list, name='projects_list'),
-    # path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
-    # path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    #path('', views.index),
+    #path('projects/', views.projects_list, name='projects_list'),
+    #path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+    #path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
 
     path('', views.IndexView.as_view(), name='index'),
     path('projects/', views.ProjectsListView.as_view(), name='projects_list'),
